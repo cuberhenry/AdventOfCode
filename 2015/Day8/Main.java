793fbcbb1818cf595cbde8055509ea548db8bd22
cuberhenry <henryanderson@cedarville.edu>
2023-05-03 19:59:11 -0400

    Days 8 and 9

---
+++ 2015/Day8/Main.java
@@ -0,0 +1,93 @@
+/*
+Henry Anderson
+Advent of Code 2015 Day 8 https://adventofcode.com/2015/day/8
+Input: https://adventofcode.com/2015/day/8/input
+1st command line argument is which part of the daily puzzle to solve
+2nd command line argument is the file name of the input, defaulted to
+    "input.txt"
+*/
+import java.util.*;
+import java.io.*;
+public class Main {
+    // The desired problem to solve
+    static int PART;
+    static Scanner sc;
+    // The file containing the puzzle input
+    static String FILE_NAME = "input.txt";
+    public static void main(String args[]) {
+        if (args.length < 1 || args.length > 2){
+            System.out.println("Wrong number of arguments");
+            return;
+        }
+        // Take in the part and file name
+        try {
+            PART = Integer.parseInt(args[0]);
+        } catch (Exception e){}
+        if (!(PART == 1 || PART == 2)){
+            System.out.println("Part can only be 1 or 2");
+            return;
+        }
+        if (args.length == 2){
+            FILE_NAME = args[1];
+        }
+        try {
+            sc = new Scanner(new File(FILE_NAME));
+        }catch (Exception e){
+            System.out.println("File not found");
+            return;
+        }
+        // The number of characters in the input
+        int total = 0;
+        // The number of characters of the encoding
+        int size = 0;
+
+        // Take in each line of input
+        while (sc.hasNext()){
+            // Take in the next line
+            String line = sc.nextLine();
+            // Increase total by the number of characters of this line
+            total += line.length();
+
+            // Add six for the quotation marks
+            if (PART == 2){
+                size += 6;
+            }
+
+            // Loop through every inner character
+            for (int i=1; i<line.length()-1; ++i){
+                // Increase the size
+                ++size;
+
+                // Part 1 finds the difference in actual characters to saved characters
+                if (PART == 1){
+                    // If there's an escape character
+                    if (line.charAt(i) == '\\'){
+                        // They count as one
+                        ++i;
+                        // An x indicates skipping more characters
+                        if (line.charAt(i) == 'x'){
+                            i += 2;
+                        }
+                    }
+                }
+
+                // Part 2 finds the number of characters required to encode the input
+                if (PART == 2){
+                    // Every \ and " requires an escape
+                    if (line.charAt(i) == '\\' || line.charAt(i) == '"'){
+                        ++size;
+                    }
+                }
+            }
+        }
+
+        // Print the answer
+        if (PART == 1){
+            System.out.println(total - size);
+        }
+
+        if (PART == 2){
+            System.out.println(size - total);
+        }
+    }
+}
---
+++ 2015/Day9/Main.java
@@ -0,0 +1,142 @@
+/*
+Henry Anderson
+Advent of Code 2015 Day 9 https://adventofcode.com/2015/day/9
+Input: https://adventofcode.com/2015/day/9/input
+1st command line argument is which part of the daily puzzle to solve
+2nd command line argument is the file name of the input, defaulted to
+    "input.txt"
+*/
+import java.util.*;
+import java.io.*;
+public class Main {
+    // The desired problem to solve
+    static int PART;
+    static Scanner sc;
+    // The file containing the puzzle input
+    static String FILE_NAME = "input.txt";
+    public static void main(String args[]) {
+        if (args.length < 1 || args.length > 2){
+            System.out.println("Wrong number of arguments");
+            return;
+        }
+        // Take in the part and file name
+        try {
+            PART = Integer.parseInt(args[0]);
+        } catch (Exception e){}
+        if (!(PART == 1 || PART == 2)){
+            System.out.println("Part can only be 1 or 2");
+            return;
+        }
+        if (args.length == 2){
+            FILE_NAME = args[1];
+        }
+        try {
+            sc = new Scanner(new File(FILE_NAME));
+        }catch (Exception e){
+            System.out.println("File not found");
+            return;
+        }
+        // All lines of input
+        ArrayList<String[]> input = new ArrayList<>();
+        // The names of all of the cities
+        ArrayList<String> cities = new ArrayList<>();
+
+        // Take in all of the lines of input
+        while (sc.hasNext()){
+            // Split them
+            String[] line = sc.nextLine().split(" ");
+            // If a city isn't in the list of cities, add it
+            if (!cities.contains(line[0])){
+                cities.add(line[0]);
+            }
+            if (!cities.contains(line[2])){
+                cities.add(line[2]);
+            }
+            // Add the line to the input
+            input.add(line);
+        }
+
+        // The array of distances between cities
+        int[][] dist = new int[cities.size()][cities.size()];
+        // The maximum distance
+        int maxDist = 0;
+
+        // Loop through every line of input
+        for (String[] line : input){
+            // Grab the distance
+            int distance = Integer.parseInt(line[4]);
+            // Add the distance bidirectionally
+            dist[cities.indexOf(line[0])][cities.indexOf(line[2])] = distance;
+            dist[cities.indexOf(line[2])][cities.indexOf(line[0])] = distance;
+            // If this distance is better, save it
+            if (distance > maxDist){
+                maxDist = distance;
+            }
+        }
+
+        // The answer to the problem
+        int best = Integer.MIN_VALUE;
+
+        // Part 1 finds the shortest distance required to visit all cities
+        if (PART == 1){
+            best = Integer.MAX_VALUE;
+        }
+
+        // Part 2 finds the longest distance to do the same
+        if (PART == 2){
+            best = 0;
+        }
+
+        // The sets of possible paths
+        Stack<String> states = new Stack<>();
+        // Add each city as a possible starting city
+        for (String city : cities){
+            states.push("0 "+city);
+        }
+
+        // Continue until all paths have been searched
+        while (!states.isEmpty()){
+            // Take the current state
+            String line = states.pop();
+            String[] currState = line.split(" ");
+            // Grab the distance
+            int distance = Integer.parseInt(currState[0]);
+
+            // Prunes states whose distances are already longer than the best solution
+            if (PART == 1){
+                if (distance >= best){
+                    continue;
+                }
+            }
+
+            // Prune states whose distances can't beat the current max no matter what
+            if (PART == 2){
+                if (distance + (cities.size()-currState.length+1)*maxDist <= best){
+                    continue;
+                }
+            }
+
+            // If it survived the pruning and it's a finished path, it's the new best
+            if (currState.length-1 == cities.size()){
+                best = distance;
+                continue;
+            }
+
+            // Loop through every city
+            for (int i=0; i<cities.size(); ++i){
+                // If it's unvisited
+                if (!line.contains(cities.get(i))){
+                    // Create a new distance
+                    String newState = ""+(distance+dist[cities.indexOf(currState[1])][i]);
+                    // Add all of the visited cities including this new one
+                    newState += " " + cities.get(i) + line.substring(line.indexOf(' '));
+                    // Add the path to states
+                    states.push(newState);
+                }
+            }
+        }
+
+        // Print the answer
+        System.out.println(best);
+    }
+}
